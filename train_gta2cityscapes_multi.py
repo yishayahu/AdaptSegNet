ed69edb8f6d3f17cfa4088577992428a6ec194d2
Yishayahu <ishayahu156@gmail.com>
2022-02-08 14:30:30 +0200

    pl

--- train_gta2cityscapes_multi.py
+++ train_gta2cityscapes_multi.py
@@ -531,6 +531,8 @@ def train_clustering(model,optimizer,scheduler,trainloader,targetloader,val_ds,t
             model.get_bottleneck = True
         model.train()
         if i_iter % config.epoch_every == 0 and i_iter != 0:
+            trainloader_iter = iter(trainloader)
+            targetloader_iter = iter(targetloader)
             source_clusters = []
             target_clusters = []
             if config.use_accumulate_for_loss:
