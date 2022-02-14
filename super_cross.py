796e6ae1626a28dd740929b0f68b3af7b11260b4
Yishayahu <ishayahu156@gmail.com>
2022-02-15 00:41:14 +0200

    no rand run all

--- super_cross.py
+++ super_cross.py
@@ -144,7 +144,7 @@ def run_cross_validation(experiments, combs, only_stats=False):
 
 
 def main():
-    experiments = ['clustering_finetune','their','adaBN']
+    experiments = ['jdot']
     combs = list(itertools.permutations(range(6), 2))
     # combs = [(1,3),(0, 4), (3, 1), (2, 5), (2, 3)]
     run_cross_validation(only_stats=False, experiments=experiments, combs=combs)
